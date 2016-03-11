3843fd7b0e469ca3813904fa02c2dac68af3b1a6
wuzhuanlei <wuzhuanlei@sina.com>
2016-03-11 14:13:17 +0800

    提交新加文件

---
+++ Controllers/SetupController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeOA.Controllers
+{
+    public class SetupController : Controller
+    {
+        //
+        // GET: /Index/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult News() {
+            return View();
+        }
+
+        public ActionResult Product() {
+            return View();
+        }  
+
+        public ActionResult About() {
+            return View();
+        } 
+
+        public ActionResult Help() {
+            return View();
+        }
+
+    }
+}
---
+++ Models/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeOA.Models {
+    /// <summary>
+    /// 首页
+    /// </summary>
+    public class IndexModel {
+
+    }
+}
